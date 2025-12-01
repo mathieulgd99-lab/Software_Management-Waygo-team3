cc6f8f1533ffa37b5c9eb8d88f085abed1b653cf
PaulSimonVorderbruegge20250025S <Simon.VP250025S@sis.hust.edu.vn>
2025-12-01 10:28:40 +0700

    added escape_&_init.js

---
+++ escape_&_init.js
@@ -0,0 +1,17 @@
+function escapeHtml(str) {
+  return String(str).replace(/[&<>"']/g, m => ({'&':'&amp;','<':'&lt;','>':'&gt;','"':'&quot;',"'":'&#39;'})[m]);
+}
+
+
+document.addEventListener("DOMContentLoaded", () => {
+  initMap();
+  populateCountryFilter();
+  displayFavorites();
+  displayItinerary();
+  loadBudget();
+  applyProfileUI();
+
+  const results = document.getElementById("results");
+  if (results) results.innerHTML = `<p class="muted">🔍
+    Use the search box above to find destinations!</p>`;
+});
